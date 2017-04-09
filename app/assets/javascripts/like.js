0bb5b22a441d841f81ac39387a80383fd70afd05
David <daruz@uc.cl>
2017-04-09 08:22:02 -0300

    solve problem with like and match

--- app/assets/javascripts/like.js
+++ app/assets/javascripts/like.js
@@ -1,10 +1,15 @@
 $(function(){
     $('.button.button1').click(function (e) {
 
-        // $.post('new_like', {game_id: this.id})
-        // 
+        //$.post('new_like', {game_id: this.id})
+         console.log("se esta imprimiendo");
         // identifier = this.id
         //
+        $.ajax({
+          type: "POST",
+          url: "new_like",
+          data: {game_id: this.id}
+        })
         //
         // $.ajax({
         //     type: "PUT",
